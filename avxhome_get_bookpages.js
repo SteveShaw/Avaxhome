334cdbc5e99d375c60d5cee102d319046e4254fe
unknown <jshao@jshaows.win.itginc.com>
2016-07-15 10:54:09 -0500

    download books javascript

---
+++ avxhome_get_bookpages.js
@@ -0,0 +1,116 @@
+//viewportSize being the actual size of the headless browser
+
+//the clipRect is the portion of the page you are taking a screenshot of
+//page.clipRect = { top: 0, left: 0, width: 1024, height: 768 };
+//the rest of the code is the same as the previous example
+var wp = require('webpage')
+var page = wp.create();
+var fs = require('fs')
+var page_root = 'http://avxhome.in/ebooks/programming_development/pages/'
+//var page_root = 'http://avxhome.in/ebooks/economics_finances/pages/'
+//var page_root = 'http://avxhome.in/ebooks/science_books/math/pages/'
+var url = page_root + 1
+var url_root = 'http://avxhome.in'
+var urls = []
+var count = 0
+var topPageCount = 0;
+var topPageUrls = []
+var page_contents = []
+var isTopPage = false
+var num_of_pages = 20;
+
+// var saveDirPath = 'page_1/'
+// fs.makeDirectory( saveDirPath )
+
+for(var i = 0;i < num_of_pages; ++i)
+{
+    var idx = i+1;
+    console.log( page_root+idx )
+    topPageUrls.push(page_root + idx)
+}
+
+function OpenTopPage()
+{
+    var bookUrls = page.evaluate( 
+        function()
+        {
+           return [].map.call(
+            document.querySelectorAll('div.col-md-12.article a.title-link'), 
+                    function(a) 
+                   {
+                      return a.getAttribute('href') 
+                   }
+            )
+        }
+    )
+    
+    var num_links = bookUrls.length 
+      
+    for(var i = 0;i < num_links;++i)
+    {
+       var bookUrl = bookUrls[i]
+       if(bookUrl.length > 0 )
+       {
+           urls.push(url_root + bookUrls[i])   
+       }
+    }
+    
+    console.log( 'found books = ' + urls.length )
+}
+
+function OpenNextTopPage()
+{
+    page.close()
+    
+    ++topPageCount;
+    
+    if(topPageCount >= num_of_pages)
+    {
+        console.log(' Final Top page is reached, will visit each child page to get book ')
+        count = 0;
+
+        fs.write("books.txt", urls.join('\n'), {mode: 'w', charset: 'UTF-8'})
+        
+        phantom.exit()
+    }
+    
+    console.log(' page count = ' + topPageCount)
+    
+    page = wp.create()
+    
+    page.open(topPageUrls[topPageCount], function( status )
+        {
+           console.log("visit "+ topPageUrls[topPageCount] + ' Status:' + status )
+           
+           OpenTopPage()
+           
+           window.setTimeout(OpenNextTopPage, 500)          
+        }
+    )
+}
+
+
+phantom.onError = function(msg, trace) {
+  var msgStack = ['PHANTOM ERROR: ' + msg];
+  if (trace && trace.length) {
+    msgStack.push('TRACE:');
+    trace.forEach(function(t) {
+      msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
+    });
+  }
+  console.error(msgStack.join('\n'));
+  phantom.exit(1);
+};
+
+page.open( topPageUrls[topPageCount], function(status){
+  
+    console.log("visit "+ topPageUrls[topPageCount] + ' Status:' + status )
+    
+    OpenTopPage()
+ 
+    window.setTimeout(OpenNextTopPage, 500)
+})
+
+
+
+
---
+++ load_books.js
@@ -0,0 +1,157 @@
+var fs = require('fs');
+var wp = require('webpage')
+var page = wp.create();
+
+var content = fs.read('books.txt');
+
+var title_filename = 'titles.txt'
+
+var page_contents = []
+var book_titles = []
+
+var limits = 45
+
+console.log('read data:', content);
+
+var links = content.split('\n')
+console.log(links.length)
+var count = 0;
+var retry = false;
+
+function PadZeros(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+function AddToPageContents( title, inner )
+{
+    page_contents.push('<div>')
+    page_contents.push('<h2>'+ title + '</h2>')
+    page_contents.push( inner )
+    page_contents.push('</div>')
+    page_contents.push('<hr>')
+}
+
+function SavePageContents()
+{
+    var idx = count / limits
+    var outPath = PadZeros( idx, 3 ) + '.html'
+    console.log('Save to "' + outPath + '"')
+    fs.write(outPath, page_contents.join('\n'), {mode: 'w', charset: 'UTF-8'})
+    page_contents = []
+}
+
+function RetrieveBookContent( )
+{
+    var titles = page.evaluate( 
+        function()
+        {
+           return [].map.call(
+            document.querySelectorAll('div.col-md-12.article h1.title-link'), 
+                    function(obj) 
+                   {
+                      return obj.innerText
+                   }
+            )
+        }
+    )
+    
+    var inners = page.evaluate( 
+        function()
+        {
+           return [].map.call(
+            document.querySelectorAll('div.col-md-12.article'), 
+                    function(obj) 
+                   {
+                      return obj.innerHTML
+                   }
+            )
+        }
+    )
+
+    console.log( 'found number of titles = ' + titles.length )
+    
+    if( titles.length > 0 && inners.length > 0 )
+    {
+        console.log(titles[0])
+        console.log('found inners length = ' + inners.length)
+        AddToPageContents( titles[0], inners[0] )
+        book_titles.push( titles[0] )
+    }
+    else
+    {
+        console.log('Error: Missing div.col-md-12.article')
+        AddToPageContents( 'Untitled', '<div><h2>NONE</h2>'+'<a href="'+ url 
+                            +'" target="_blank"><span style="color: Red;font-size: 300\%">'
+                            +url+'</span></a>'+'</div>' )
+    }
+}
+
+function LoadPage()
+{
+    page.close()
+    
+    if( !retry )
+    {
+        ++count;
+        
+        if(count >= links.length)
+        {
+            SavePageContents()
+            console.log('Save titles')
+            fs.write(title_filename, book_titles.join('\n'), {mode: 'w', charset: 'UTF-8'})
+            phantom.exit()
+        }
+        
+        if( count % limits == 0 )
+        {
+            SavePageContents()
+        }
+    }
+    else
+    {
+        console.log('Retry...')
+    }
+    
+    page = wp.create()
+    
+    page.open(links[count], function(status) {
+        
+            console.log('current count = ' + count)
+            console.log( 'visit: '+ links[count] + ' Status: ' + status )
+            
+            if( status != 'success' )
+            {
+                retry  = true;
+            }
+            else
+            {
+                retry = false;
+            }
+            
+            RetrieveBookContent()
+            window.setTimeout(LoadPage, 200);
+        }
+    )
+}
+
+page.open(links[count], function(status){
+            
+            console.log('current count = ' + count)
+            console.log( 'visit: '+ links[count] + ' Status: ' + status )
+            
+            if( status != 'success' )
+            {
+                retry  = true;
+            }
+            else
+            {
+                retry = false;
+            }
+            
+            RetrieveBookContent()
+            window.setTimeout(LoadPage, 200);
+        }
+    )
+
