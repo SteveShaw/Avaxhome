daadeddb590d7158c0d25dde7d8db47031cc2dfb
SteveShaw <steve.shao@yahoo.com>
2016-10-26 14:12:45 -0500

    updated

--- LeetCode_Solutions.cpp
+++ LeetCode_Solutions.cpp
@@ -463,3 +463,24 @@ public:
         
     }
 };
+
+/*
+421. Maximum XOR of Two Numbers in an Array
+Given a non-empty array of numbers, a[0], a[1,] a[2], … , a[n-1], where 0 ≤ a[i] < 2^31.
+Find the maximum result of a[i] XOR a[j], where 0 <= i, j < n.
+Could you do this in O(n) runtime?
+
+Example:
+
+Input: [3, 10, 5, 25, 2, 8]
+
+Output: 28
+
+Explanation: The maximum result is 5 ^ 25 = 28.
+
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+    }
+};
+*/
