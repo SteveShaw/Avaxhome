5aa81b75f3882bf4149e36fdd7574de16073df58
SS <steve.shao@yahoo.com>
2017-02-26 23:48:17 -0500

    updated

--- 080.cpp
+++ 080.cpp
@@ -244,7 +244,9 @@ public:
 
 //58. Length of Last Word
 /*
-Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
+
+return the length of last word in the string.
 
 If the last word does not exist, return 0.
 
@@ -11832,5 +11834,67 @@ class Solution {
 public:
     int minTotalDistance(vector<vector<int>>& grid)
 		{
+			int rows = grid.size();
+			int cols = grid[0].size();
+			
+			for(int )
 		}
 };
+
+//<--> 297. Serialize and Deserialize Binary Tree
+/*
+Serialization is the process of converting a data structure 
+
+or object into a sequence of bits so that it can be stored in a file or memory buffer, 
+
+or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary tree. 
+
+There is no restriction on how your serialization/deserialization algorithm should work. 
+
+You just need to ensure that a binary tree can be serialized to a string and 
+
+this string can be deserialized to the original tree structure.
+
+For example, you may serialize the following tree
+
+    1
+   / \
+  2   3
+     / \
+    4   5
+as "[1,2,3,null,null,4,5]", just the same as how LeetCode OJ serializes a binary tree. 
+
+You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+
+Note: Do not use class member/global/static variables to store states. 
+
+Your serialize and deserialize algorithms should be stateless.
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));
