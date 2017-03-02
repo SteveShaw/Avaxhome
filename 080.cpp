0de6c40330b7bdc2704d9a569968d9926a3d4506
SteveShaw <steve.shao@yahoo.com>
2017-03-02 08:33:41 -0600

    updated

--- 080.cpp
+++ 080.cpp
@@ -13980,6 +13980,179 @@ private:
 	vector<int> N;
 };
 
+//<--> 308. Range Sum Query 2D - Mutable
+/*
+Given a 2D matrix matrix, find the sum of the elements
+inside the rectangle defined by its upper left corner (row1, col1)
+and lower right corner (row2, col2).
+
+Example:
+Given matrix = [
+[3, 0, 1, 4, 2],
+[5, 6, 3, 2, 1],
+[1, 2, 0, 1, 5],
+[4, 1, 0, 1, 7],
+[1, 0, 3, 0, 5]
+]
+
+sumRegion(2, 1, 4, 3) -> 8
+update(3, 2, 2)
+sumRegion(2, 1, 4, 3) -> 10
+Note:
+1. The matrix is only modifiable by the update function.
+2. You may assume the number of calls to update and sumRegion function is distributed evenly.
+3. You may assume that row1 <= row2 and col1 <= col2.
+*/
+/*Binary Index Tree Method: 2D*/
+class NumMatrix {
+public:
+	NumMatrix( vector<vector<int>> &matrix )
+	{
+		if ( matrix.empty() || matrix[0].empty() )
+		{
+			return;
+		}
+
+		BIT.resize( matrix.size() + 1, vector<int>( matrix[0].size() + 1, 0 ) );
+		N.resize( matrix.size() + 1, vector<int>( matrix[0].size() + 1, 0 ) );
+
+		int rows = matrix.size();
+		int cols = matrix[0].size();
+
+		for ( int i = 0; i < rows; ++i )
+		{
+			for ( int j = 0; j < cols; ++j )
+			{
+				update( i, j, matrix[i][j] );
+			}
+		}
+	}
+
+	void update( int row, int col, int val )
+	{
+		int diff = val - N[row + 1][col + 1];
+		int rows = N.size();
+		int cols = N[0].size();
+
+		for ( int i = row + 1; i < rows; i += (i&-i) )
+		{
+			for ( int j = col + 1; j < cols; j += (j&-j) )
+			{
+				BIT[i][j] += diff;
+			}
+		}
+
+		N[row + 1][col + 1] = val;
+	}
+
+	int sumRegion( int row1, int col1, int row2, int col2 )
+	{
+		return getSum( row2 + 1, col2 + 1 ) - getSum( row2 + 1, col1 ) - getSum( row1, col2 + 1 ) + getSum( row1, col1 );
+	}
+
+	int getSum( int row, int col )
+	{
+		int sum = 0;
+
+		for ( int i = row; i > 0; i -= (i&-i) )
+		{
+			for ( int j = col; j > 0; j -= (j&-j) )
+			{
+				sum += BIT[i][j];
+			}
+		}
+
+		return sum;
+	}
+
+private:
+	vector<vector<int>> BIT;
+	vector<vector<int>> N;
+};
+
+//<--> 309. Best Time to Buy and Sell Stock with Cooldown
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit.
+
+You may complete as many transactions as you like
+(i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
+
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+Example:
+
+prices = [1, 2, 3, 0, 2]
+maxProfit = 3
+transactions = [buy, sell, cooldown, buy, sell]
+*/
+
+/*
+The series of problems are typical dp.
+
+The natural states for this problem is the 3 possible transactions:
+
+buy, sell, rest. Here rest means no transaction on that day (aka cooldown).
+
+Then the transaction sequences can end with any of these three states.
+
+For each of them we make an array, buy[n], sell[n] and rest[n].
+
+buy[i] means before day i what is the maxProfit for any sequence end with buy.
+
+sell[i] means before day i what is the maxProfit for any sequence end with sell.
+
+rest[i] means before day i what is the maxProfit for any sequence end with rest.
+
+Then we want to deduce the transition functions for buy sell and rest. By definition we have:
+
+buy[i]  = max(rest[i-1] - price, buy[i-1])
+
+sell[i] = max(buy[i-1] + price, sell[i-1])
+
+rest[i] = max(sell[i-1], buy[i-1], rest[i-1])
+
+Where price is the price of day i. All of these are very straightforward. They simply represents :
+
+(1) We have to `rest` before we `buy` and
+(2) we have to `buy` before we `sell`
+
+One tricky point is how do you make sure you sell before you buy,
+
+since from the equations it seems that [buy, rest, buy] is entirely possible.
+
+Well, the answer lies within the fact that buy[i] <= rest[i]
+
+which means rest[i] = max(sell[i-1], rest[i-1]).
+
+That made sure [buy, rest, buy] is never occurred.
+
+A further observation is that rest[i] <= sell[i] is also true therefore
+
+rest[i] = sell[i-1]
+
+Substitute this in to buy[i] we now have 2 functions instead of 3:
+
+buy[i] = max(sell[i-2]-price, buy[i-1])
+
+sell[i] = max(buy[i-1]+price, sell[i-1])
+
+This is better than 3, but
+
+we can do even better
+
+Since states of day i relies only on i-1 and i-2
+
+we can reduce the O(n) space to O(1). And here we are at our final solution:
+*/
+class Solution {
+public:
+	int maxProfit( vector<int>& prices )
+	{
+	}
+};
+
 //<--> 310. Minimum Height Trees
 /*
 For a undirected graph with tree characteristics, 
@@ -14106,6 +14279,5 @@ class Solution {
 public:
 	int maxCoins(vector<int>& nums)
 	{
-
 	}
 };
