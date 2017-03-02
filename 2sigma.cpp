fce831f1816fe7b9495461ba01f96a9bbc1c1fa8
SteveShaw <steve.shao@yahoo.com>
2017-03-02 16:50:25 -0600

    added 2sigma.cpp

---
+++ 2sigma.cpp
@@ -0,0 +1,60 @@
+/*
+Phone Screen - someone from infrastructure team 1, 
+
+resume 2, Given an application that a class with big data, 
+
+so creating an instance of this class is slow, how to solve it? 
+
+3, Someone implement hash table and it is slow, why? 
+
+4, use hash table to store data, but there is much more data than the machine's RAM, how to deal with that? 
+
+add one more machine, rehash and reconstruct the hash table 
+
+5, A application involves with multiple machines and it is slow, 
+
+figure out why can use matrix to measure the latency and throughput of each machine 
+
+6, process & thread 7, throughput & latency 
+
+On site: （悲剧地没只撑到中午饭 = =） 
+
+1, write a function to determine if a given long num is power of 4 
+
+Using a random number iterator to crate a iterator that only return multiply of 5
+
+2, given a 2 dimension matrix, set all cubes to 1 or 0 according to the number of neighbors' value of 
+
+the cube at the same time (game of life) 
+
+If we have multiple processor, 
+
+how do u improve that If we have multiple machines, how do we apply 
+
+3, Java find bug, multimap implements a map interface and inherits other abstract map
+*/
+
+/*
+I have a one hour phone screen. 
+
+Interviewer spent 3 minute measures himself and then told me that there will be 6 knowledge questions 
+
+plus one algorithm implementation. 
+
+1. Describe a design pattern (eg factory pattern, MVC)  
+
+2. Describe a hashtable, How does it work? 
+
+3. How to deal with collisions in a hashtable? 
+
+4. Difference between Throughput and Latency   
+
+5. Difference between a process and a thread and difference between their communication  
+
+6. Given a stream of numbers, how would you calculate the median (used 2 heaps, question is on Leetcode) 
+
+*/
+
+/*
+http://www.themianjing.com/?s=two+sigma
+*/
